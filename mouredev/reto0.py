bd4b164a1feff7d0f5186b5a69597de769f7f359
Xerchio <xerchion@gmail.com>
2023-10-13 09:57:33 +0200

    by order

--- mouredev/reto0.py
+++ mouredev/reto0.py
@@ -10,33 +10,52 @@
 # - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
 
 
-# v1.0
-def fizz_buzz_numbers():
+# v4.1
+def fizz_buzz_numbers_41() -> None:
+    def check(num: int, target: int, message: str) -> str:
+        return message if num % target == 0 else ""
+
+    for i in range(1, 101):
+        fizz = check(i, 3, "fizz")
+        buzz = check(i, 5, "buzz")
+        print(fizz + buzz if fizz or buzz else i)
+
+
+# v4.0
+def fizz_buzz_numbers_4():
+    def oper(num, target, message):
+        return message if num % target == 0 else ""
+
+    for i in range(1, 101):
+        fizz = oper(i, 3, "fizz")
+        buzz = oper(i, 5, "buzz")
+        print(fizz + buzz if fizz or buzz else i)
+
+
+# v3.2
+def fizz_buzz_numbers_32():
+    def fizz(num):
+        return "fizz" if num % 3 == 0 else ""
+
+    def buzz(num):
+        return "buzz" if num % 5 == 0 else ""
+
+    for i in range(1, 101):
+        print(fizz(i) + buzz(i) if fizz(i) or buzz(i) else i)
+
+
+# v3.1
+def fizz_buzz_numbers_31():
+    def fizz(num):
+        return "fizz" if num % 3 == 0 else ""
+
+    def buzz(num):
+        return "buzz" if num % 5 == 0 else ""
+
     for i in range(1, 101):
         result = i
-        if i % 3 == 0:
-            result = "fizz"
-        if i % 5 == 0:
-            result = "buzz"
-        if i % 3 == 0 and i % 5 == 0:
-            result = "fizzbuzz"
+        result = fizz(i) + buzz(i) if fizz(i) or buzz(i) else i
         print(result)
-
-
-# fizz_buzz_numbers()
-
-
-# v2
-def fizz_buzz_numbers_2():
-    for i in range(1, 101):
-        result = i
-        result = "fizz" if i % 3 == 0 else result
-        result = "fizz" if i % 3 == 0 else result
-        result = "fizzbuzz" if i % 3 == 0 and i % 5 == 0 else result
-        print(result)
-
-
-# fizz_buzz_numbers_2()
 
 
 # v3
@@ -58,64 +77,23 @@ def fizz_buzz_numbers_3():
         print(result)
 
 
-# fizz_buzz_numbers_3()
-
-
-# v3.1
-def fizz_buzz_numbers_31():
-    def fizz(num):
-        return "fizz" if num % 3 == 0 else ""
-
-    def buzz(num):
-        return "buzz" if num % 5 == 0 else ""
-
+# v2
+def fizz_buzz_numbers_2():
     for i in range(1, 101):
         result = i
-        result = fizz(i) + buzz(i) if fizz(i) or buzz(i) else i
+        result = "fizz" if i % 3 == 0 else result
+        result = "buzz" if i % 5 == 0 else result
+        result = "fizzbuzz" if i % 3 == 0 and i % 5 == 0 else result
+
+
+# v1.0
+def fizz_buzz_numbers():
+    for i in range(1, 101):
+        result = i
+        if i % 3 == 0:
+            result = "fizz"
+        if i % 5 == 0:
+            result = "buzz"
+        if i % 3 == 0 and i % 5 == 0:
+            result = "fizzbuzz"
         print(result)
-
-
-fizz_buzz_numbers_31()
-
-
-# v3.2
-def fizz_buzz_numbers_32():
-    def fizz(num):
-        return "fizz" if num % 3 == 0 else ""
-
-    def buzz(num):
-        return "buzz" if num % 5 == 0 else ""
-
-    for i in range(1, 101):
-        print(fizz(i) + buzz(i) if fizz(i) or buzz(i) else i)
-
-
-# fizz_buzz_numbers_32()
-
-
-# v4.0
-def fizz_buzz_numbers_4():
-    def oper(num, target, message):
-        return message if num % target == 0 else ""
-
-    for i in range(1, 101):
-        fizz = oper(i, 3, "fizz")
-        buzz = oper(i, 5, "buzz")
-        print(fizz + buzz if fizz or buzz else i)
-
-
-# fizz_buzz_numbers_4()
-
-
-# v4.1
-def fizz_buzz_numbers_41() -> None:
-    def check(num: int, target: int, message: str) -> str:
-        return message if num % target == 0 else ""
-
-    for i in range(1, 101):
-        fizz = check(i, 3, "fizz")
-        buzz = check(i, 5, "buzz")
-        print(fizz + buzz if fizz or buzz else i)
-
-
-fizz_buzz_numbers_41()
